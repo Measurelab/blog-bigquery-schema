d6b90915877d158b1fd56a16962319a4deb7abb8
lacerogers20 <63338825+lacerogers20@users.noreply.github.com>
2021-03-11 20:12:02 +0000

    Update ddl-code1.sql

--- ddl-your-new-best-friend/ddl-code1.sql
+++ ddl-your-new-best-friend/ddl-code1.sql
@@ -1,6 +1,7 @@
 SELECT 
 date,
 count(distinct(visitid)) as unique_visits
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`  #public dataset
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*` 
+WHERE _table_suffix = '20170801'   # use date_sub to programmatically select your table
 Group by 1
 order by 1
