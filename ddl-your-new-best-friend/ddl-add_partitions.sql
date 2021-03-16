9f0ff6993d33471de83133a25ace9d5ee744b974
lacerogers20 <63338825+lacerogers20@users.noreply.github.com>
2021-03-16 20:46:39 +0000

    Create ddl-add_partitions.sql

---
+++ ddl-your-new-best-friend/ddl-add_partitions.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE TABLE
+  `testing.insert` (date DATE OPTIONS( description='date' ),
+    unique_visits INT64 OPTIONS( description='counts' ))
+PARTITION BY
+  date OPTIONS ( description="A table with a date partition" ) AS (
+  SELECT
+    DATE(TIMESTAMP_SECONDS( visitStartTime)) AS date,
+    COUNT(DISTINCT(visitid)) AS unique_visits
+  FROM
+    `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+  WHERE
+    _table_suffix = '20170801'
+  GROUP BY
+    1);
